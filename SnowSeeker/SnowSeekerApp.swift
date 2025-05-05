7da3d2fb52ce52f330258201aa1f4a6b122a1b2f
xy7-ux <ostrowskiszkola8@gmail.com>
2025-05-05 16:43:54 +0200

    Initial Commit

---
+++ SnowSeeker/SnowSeekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowSeekerApp.swift
+//  SnowSeeker
+//
+//  Created by Patryk Ostrowski on 05/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct SnowSeekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
